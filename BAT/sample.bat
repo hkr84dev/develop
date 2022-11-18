6d7a2a355f68d4a209836e6baec4daf4a1e7dc5a
hkr84 <hkr84dev@gmail.com>
2022-11-18 16:56:00 +0900

    update

--- BAT/sample.bat
+++ BAT/sample.bat
@@ -1,4 +1,15 @@
-rem
+@ECHO OFF
+REM よく使用するコマンド集
 
-rem
-rem 本日の日付のフォルダ、ファイルを生成する
+REM バッチファイルの実行ディレクトリに移動する
+CD %~DP0
+
+REM 
+
+REM 本日の日付のフォルダ、ファイルを生成する
+SET TODAY=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%
+SET CTIME=%TIME:~0,2%%TIME:~3,2%
+
+MKDIR .\%TODAY% 2>NUL
+ECHO. >> .\%TODAY%\%TODAY%.txt
+ECHO %CTIME: =0% >> .\%TODAY%\%TODAY%.txt
